66e55802587e99580755286862605b2e5a99ddeb
MES4game <mes4.game@gmail.com>
2025-10-07 07:09:51 +0200

    reset repo

--- src/shared/models/common/props.model.ts
+++
@@ -1,6 +0,0 @@
-import { CSSProperties } from "react";
-
-export interface ComponentProps {
-    readonly className?: string;  // eslint-disable-line @typescript-eslint/naming-convention
-    readonly style?    : CSSProperties;
-}
