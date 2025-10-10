94e971f64998a2ca7742134ecb3b604792e103d2
VoLtRL <raphael.lecaille@gmail.com>
2025-10-10 08:42:27 +0200

    feat(club-17): add clubs page and route
    - created 'src/ui/pages/clubs' folder
    - added `clubs.page.tsx` and 'clubs.section.tsx'
    - updated 'app.tsx' to include club route

--- src/app.tsx
+++ src/app.tsx
@@ -33,6 +33,7 @@ const App: FC = (): ReactNode => {
                         <Route path="/members" element={<MembersPage />} />
                         <Route path="/club" element={<ClubPage />} />
                         <Route path="/members" element={<MembersPage />} />
+                        <Route path="/club" element={<ClubPage />} />
                         <Route path="*" element={<NotFoundPage />} />
                         <Route path="/club/:name" element={<DetailedClubPage />} />
                     </Routes>
