e17aa6e2b17324450c5fa82c59970a49d56665f3
VoLtRL <raphael.lecaille@gmail.com>
2025-10-10 08:42:27 +0200

    feat(club-17): add clubs page and route
    - created 'src/ui/pages/clubs' folder
    - added `clubs.page.tsx` and 'clubs.section.tsx'
    - updated 'app.tsx' to include club route

--- src/app.tsx
+++ src/app.tsx
@@ -12,6 +12,8 @@ const NotFoundPage     = lazy(() => { return import("@/ui/pages/not_found.page")
 const ClubPage         = lazy(() => { return import("@/ui/pages/clubs/clubs.page"); });
 const DetailedClubPage = lazy(() => { return import("@/ui/pages/clubs/{name}/detailedclub.page"); });
 const MembersPage  = lazy(() => { return import("@/ui/pages/members/members.page"); });
+const ClubPage     = lazy(() => { return import("@/ui/pages/clubs/clubs.page"); });
+
 
 const App: FC = (): ReactNode => {
     useEffect(() => {
@@ -32,6 +34,7 @@ const App: FC = (): ReactNode => {
                         <Route path="/" element={<HomePage />} />
                         <Route path="/club" element={<ClubPage />} />
                         <Route path="/members" element={<MembersPage />} />
+                        <Route path="/club" element={<ClubPage />} />
                         <Route path="*" element={<NotFoundPage />} />
                         <Route path="/club/:name" element={<DetailedClubPage />} />
                     </Routes>
