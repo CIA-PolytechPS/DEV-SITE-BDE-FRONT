4605fd3f3967ef8da15e529e0f3b4749e8097abc
VoLtRL <raphael.lecaille@gmail.com>
2025-10-20 00:04:57 +0200

    style(club-17): update ClubCard and ClubsSection components for improved styling and layout

--- src/ui/components/clubs/clubscard.component.tsx
+++ src/ui/components/clubs/clubscard.component.tsx
@@ -6,7 +6,7 @@ const ClubCardComp: FC<Club> = ({ nom, logo, description }) => {
         <>
             <div
                 className="flex flex-col md:flex-row items-start mx-auto my-5
-            border border-white shadow-2xl rounded-3xl bg-[#323232] w-full max-w-5xl p-4
+            border border-[#F5F5F7] shadow-2xl rounded-3xl bg-[#E6E6E610] w-full max-w-5xl p-4
             hover:shadow-[0_0_20px_rgba(255,255,255,0.2)] transition-all duration-300 ease-in-out"
             >
                 <div className="flex-shrink-0 w-64 h-64 overflow-hidden">
@@ -16,13 +16,10 @@ const ClubCardComp: FC<Club> = ({ nom, logo, description }) => {
                 <div className="flex flex-col justify-between ml-2">
                     <div className="flex justify-between items-start my-0">
                         <h2 className="text-[35px] my-0 leading-none">{nom}</h2>
-
-                        <button className="bg-[#FFAA00] hover:bg-[#e2910e] text-black font-medium text-sm px-4 py-1 rounded-lg translate-y-1/8  ">
-                            More
-                        </button>
+                        <button className="hover:bg-[#e2910e] font-medium text-sm px-4 py-1 rounded-lg translate-y-1/8  ">More</button>
                     </div>
 
-                    <p className="mt-2 text-sm text-[#FFFFFFd9] leading-relaxed">{description}</p>
+                    <p className="mt-2 text-sm leading-relaxed">{description}</p>
                 </div>
             </div>
         </>
