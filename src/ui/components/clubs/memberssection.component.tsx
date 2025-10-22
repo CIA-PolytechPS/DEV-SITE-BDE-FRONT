c3806ae651f66e1ce3593cbaad0fcedd9a3f18d1
GabrielRadiguet <g.radiguet@proton.me>
2025-10-23 00:03:06 +0200

    style(home-14): styling of members' section with responsiveness

--- src/ui/components/clubs/memberssection.component.tsx
+++ src/ui/components/clubs/memberssection.component.tsx
@@ -2,18 +2,17 @@ import { FC, ReactNode } from "react";
 
 const MembersSectionComp: FC = (): ReactNode => {
     return (
-        <section>
-            <div className="flex flex-row">
-                <div>
-                    <img src="/logo512.png" alt="Ceci est une image." />
-                    <p>Source : Hello World</p>
+        <section className="flex justify-center items-center py-16 px-8">
+            <div className="flex flex-col max-w-7xl justify-between items-center gap-10 md:flex-row">
+                <div className="basis-1/3">
+                    <img className="object-contain" src="/logo512.png" alt="Ceci est une image." />
+                    <p className="text-zinc-500">Source : Hello World</p>
                 </div>
 
-                <div>
-                    <h2>BDE Polytech Paris-Saclay</h2>
-                    <p>L'adhésion au BDE vous permet d'accéder à une multitude de Clubs.</p>
+                <div className="max-w-2xl pl-6 basis-2/3">
+                    <h1 className="font-semibold text-5xl tracking-tighter mb-2">BDE Polytech Paris-Saclay</h1>
 
-                    <div>
+                    <div className="pt-4">
                         <p>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dictum bibendum eros, nec ornare dolor luctus a. Donec
                             mattis viverra dolor, sed semper libero consectetur a. Curabitur quis magna facilisis, congue ipsum non, commodo diam.
