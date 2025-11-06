9c0095af9f9d5f48f6bf4b78a12d357c908e69a7
GabrielRadiguet <g.radiguet@proton.me>
2025-11-06 15:01:45 +0000

    refactor(home-14): moving separator style to global stylesheet

--- src/ui/pages/members/members.page.tsx
+++ src/ui/pages/members/members.page.tsx
@@ -1,5 +1,4 @@
 import { FC, ReactNode, useEffect } from "react";
-import "@/ui/pages/members/members.page.css";
 
 import MembersSectionComp from "../../components/members/memberssection.component";
 import MembersListComp from "../../components/members/memberslist.component";
