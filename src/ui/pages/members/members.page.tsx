ca2dd5a07bb51e6cfd039d91a8a8234d9d91e48b
GabrielRadiguet <g.radiguet@proton.me>
2025-11-06 14:51:11 +0000

    feat|style(home-14): add members list and members card component with style

--- src/ui/pages/members/members.page.tsx
+++ src/ui/pages/members/members.page.tsx
@@ -2,6 +2,7 @@ import { FC, ReactNode, useEffect } from "react";
 import "@/ui/pages/members/members.page.css";
 
 import MembersSectionComp from "../../components/members/memberssection.component";
+import MembersListComp from "../../components/members/memberslist.component";
 
 const HomePage: FC = (): ReactNode => {
     useEffect(() => {
@@ -15,6 +16,8 @@ const HomePage: FC = (): ReactNode => {
     return (
         <>
             <MembersSectionComp />
+            <div className="separator" />
+            <MembersListComp />
         </>
     );
 };
