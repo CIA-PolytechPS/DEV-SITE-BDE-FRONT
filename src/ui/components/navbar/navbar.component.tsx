d3e38f3bc06a3e09b03c9d9f91a1236ba7ccd87e
VoLtRL <raphael.lecaille@gmail.com>
2025-11-06 08:38:02 +0100

    feat(home-28): update navbar layout and styles; add logo and links

--- src/ui/components/navbar/navbar.component.tsx
+++ src/ui/components/navbar/navbar.component.tsx
@@ -1,13 +1,12 @@
 import { FC, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faComments } from "@fortawesome/free-solid-svg-icons";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
 import { useGeneralVars } from "@/shared/contexts/common/general.context";
 import { useReRender } from "@/shared/utils/common/hook.util";
 import "@/ui/components/navbar/navbar.component.css";
 
 const NavbarComp: FC = () => {
-    const navigate = useNavigate();
     const { navbar_title } = useGeneralVars();
     const reRender = useReRender();
 
@@ -16,9 +15,17 @@ const NavbarComp: FC = () => {
 
         const unsubscribers: (() => void)[] = [];
 
-        unsubscribers.push(navbar_title.subscribe(() => { reRender(); }));
+        unsubscribers.push(
+            navbar_title.subscribe(() => {
+                reRender();
+            }),
+        );
 
-        return () => { unsubscribers.forEach((fn) => { fn(); }); };
+        return () => {
+            unsubscribers.forEach((fn) => {
+                fn();
+            });
+        };
     }, []);
 
     useEffect(() => {
@@ -26,15 +33,23 @@ const NavbarComp: FC = () => {
     });
 
     return (
-        <header id="navbar">
-            <p>Menu</p>
-            <h1 className="navbar-title">{navbar_title.current}</h1>
+        <header id="navbar" className="">
+            <span className="flex flex-auto">
+                <img src="./logo.png" />
+                <p className="mt-[revert] pl-2">BDE Polytech Paris Saclay - Université Paris Saclay</p>
+            </span>
 
-            <FontAwesomeIcon
-                icon={faComments}
-                onClick={() => { void navigate("/contact"); }}
-                className="navbar-icon"
-            />
+            <nav className="grid grid-cols-5 gap-1">
+                <Link className="text-center" to="/clubs">Clubs</Link>
+                <Link className="text-center" to="/events">Events</Link>
+                <Link className="text-center" to="/members">Members</Link>
+
+                <Link className="m-auto" to="/settings">
+                    <FontAwesomeIcon icon={faGear} />
+                </Link>
+
+                <button className="rounded-[8px] bg-black  px-3 py-2"><Link className="text-[#F7F7F7]" to="/">Home</Link></button>
+            </nav>
         </header>
     );
 };
