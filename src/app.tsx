3bb65bb6d5a178c52e74d2be17daaf00e77edd0e
VoLtRL <raphael.lecaille@gmail.com>
2025-11-06 16:17:39 +0100

    feat(club-18): add DetailedClubPage and club model

--- src/app.tsx
+++ src/app.tsx
@@ -3,12 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import LoadingComp from "@/ui/components/common/loading.component";
 import NavbarComp from "@/ui/components/navbar/navbar.component";
 import InfobarComp from "@/ui/components/infobar/infobar.component";
-import "@fontsource/inter";
+
 import "@/app.css";
 
-const HomePage     = lazy(() => { return import("@/ui/pages/home/home.page"); });
-const NotFoundPage = lazy(() => { return import("@/ui/pages/not_found.page"); });
-const MembersPage  = lazy(() => { return import("@/ui/pages/members/members.page"); });
+
+const HomePage         = lazy(() => { return import("@/ui/pages/home/home.page"); });
+const NotFoundPage     = lazy(() => { return import("@/ui/pages/not_found.page"); });
+const DetailedClubPage = lazy(() => { return import("@/ui/pages/club/detailedclub.page")})
 
 const App: FC = (): ReactNode => {
     useEffect(() => {
@@ -29,6 +30,7 @@ const App: FC = (): ReactNode => {
                         <Route path="/" element={<HomePage />} />
                         <Route path="/members" element={<MembersPage />} />
                         <Route path="*" element={<NotFoundPage />} />
+                        <Route path="/clubs/:name" element={<DetailedClubPage />} />
                     </Routes>
                 </Suspense>
             </main>
