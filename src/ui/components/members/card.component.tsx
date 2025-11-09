e119aef89f45de1ce7be894b26a44ef76d4e1ddf
GabrielRadiguet <g.radiguet@proton.me>
2025-11-09 18:54:40 +0000

    refactor|chore(home-14): fix version of react-markdown and fix MemberCard props

--- src/ui/components/members/card.component.tsx
+++ src/ui/components/members/card.component.tsx
@@ -3,7 +3,11 @@ import { Member } from "@/shared/models/member.model";
 import PersonIcon from "@mui/icons-material/Person";
 import EmailIcon from "@mui/icons-material/Email";
 
-const MemberCardComp: FC<Member> = (membre: Member) => {
+interface MemberCardProps {
+    member: Member;
+}
+
+const MemberCardComp: FC<MemberCardProps> = (props: MemberCardProps) => {
     return (
         <>
             <div
@@ -12,22 +16,22 @@ const MemberCardComp: FC<Member> = (membre: Member) => {
             hover:shadow-[0_0_20px_rgba(255,255,255,0.2)] transition-all duration-300 ease-in-out"
             >
                 <div className="flex-shrink-0 w-64 h-64 overflow-hidden">
-                    <img src={membre.photo} alt="Ceci n'est pas une image" className="w-full h-full object-contain" />
+                    <img src={props.member.photo} alt="Ceci n'est pas une image" className="w-full h-full object-contain" />
                 </div>
 
                 <div className="flex flex-col justify-between ml-2">
                     <div className="flex flex-row md:flex-col justify-between items-start my-0">
                         <div className="inline-flex items-center my-0">
                             <PersonIcon fontSize="large" className="scale(2)" sx={{ color: "#FFAA00" }} />
-                            <h2 className="text-[35px] my-0 leading-none pt-1">{membre.nom}</h2>
+                            <h2 className="text-[35px] my-0 leading-none pt-1">{props.member.nom}</h2>
                         </div>
 
-                        <h3 className="text-[25px] my-0 leading-none pt-3">{membre.fonction}</h3>
+                        <h3 className="text-[25px] my-0 leading-none pt-3">{props.member.fonction}</h3>
                     </div>
 
                     <div className="inline-flex items-center my-0 pt-2">
                         <EmailIcon sx={{ color: "#FFAA00" }} />
-                        <h3 className="text-sm text-gray-400 my-0 leading-none pl-1">{membre.email}</h3>
+                        <h3 className="text-sm text-gray-400 my-0 leading-none pl-1">{props.member.email}</h3>
                     </div>
                 </div>
             </div>
