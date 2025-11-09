659b3dc492d9088b473bc8fe5c8b20edbd36b2e2
GabrielRadiguet <g.radiguet@proton.me>
2025-11-09 17:28:01 +0000

    refactor(home-14): add of member's api and model

--- src/api/member.api.ts
+++ src/api/member.api.ts
@@ -1,19 +1,23 @@
 import { Member, mapMember } from "@/shared/models/member.model";
 
-const MEMBERS: Member[] = [
+const CLUBS: Member[] = [
+    // Replace this by the API call to gather all the clubs.
     {
+        //
         nom     : "Nom Prenom",
         photo   : "/fillingPic.gif",
         fonction: "Fonction",
         email   : "nom.prenom@universite-paris-saclay.fr",
     },
     {
+        //
         nom     : "Nom Prenom",
         photo   : "/fillingPic.gif",
         fonction: "Fonction",
         email   : "nom.prenom@universite-paris-saclay.fr",
     },
     {
+        //
         nom     : "Nom Prenom",
         photo   : "/fillingPic.gif",
         fonction: "Fonction",
@@ -24,5 +28,5 @@ const MEMBERS: Member[] = [
 export async function getMembers(): Promise<Member[]> {
     await fetch("https://data.bde-pps.fr/bde/images/logo/bde.svg");
 
-    return MEMBERS.map(mapMember);
+    return CLUBS.map((club) => { return mapMember(club); });
 }
--- src/ui/components/members/list.component.tsx
+++ src/ui/components/members/list.component.tsx
@@ -1,9 +1,8 @@
 import { FC, ReactNode, useEffect, useState } from "react";
 import { getMembers } from "@/api/member.api";
 import { Member } from "@/shared/models/member.model";
-import MemberCard from "@/ui/components/members/card.component";
+import MemberCard from "./card.component";
 import { CircularProgress } from "@mui/material";
-import { unknownToString } from "@/shared/utils/common/convert.util";
 
 const MembersListComp: FC = (): ReactNode => {
     const [members, setmembers] = useState<Member[]>([]);
@@ -11,33 +10,27 @@ const MembersListComp: FC = (): ReactNode => {
     useEffect(() => {
         getMembers()
             .then(setmembers)
-            .catch((error: unknown) => { alert("Can't retrieve Members list :" + unknownToString(error)); });
+            .catch((error: unknown) => { alert("Can't retrieve Members list :" + String(error)); });
     }, []);
 
     return (
-        <div className="w-7/8 m-[2%]">
-            <div className="w-full">
-                <h1 className="underline underline-offset-3 text-5xl font-semibold">Les Membres du Bureau</h1>
-            </div>
+        <>
+            <div className="w-4/5 m-[2%]">
+                <div className="w-full">
+                    <h1 className="underline text-5xl font-semibold">Les Membres du Bureau</h1>
+                </div>
 
-            {
-                members.length === 0
+                {members.length === 0
                     ? <CircularProgress />
                     : (
-                        <ul className="mt-[3%] columns-1 lg:columns-2">
-                            {
-                                members.map((member) => {
-                                    return (
-                                        <li>
-                                            <MemberCard member={member} />
-                                        </li>
-                                    );
-                                })
-                            }
-                        </ul>
-                    )
-            }
-        </div>
+                        <div className="mt-[3%]">
+                            {members.map((club) => {
+                                return <MemberCard key={club.nom} {...club} />;
+                            })}
+                        </div>
+                    )}
+            </div>
+        </>
     );
 };
 
