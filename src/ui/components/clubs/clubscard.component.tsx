2f1e8e5080a25ddaa3e70bd0b719ff40d032d832
VoLtRL <raphael.lecaille@gmail.com>
2026-02-10 14:11:48 +0100

    style(club-17): update styles for ClubCard, ClubsList, and Infobar components for improved aesthetics and consistency

--- src/ui/components/clubs/clubscard.component.tsx
+++ src/ui/components/clubs/clubscard.component.tsx
@@ -5,18 +5,29 @@ const ClubCardComp: FC<Club> = ({ nom, logo, description }) => {
     return (
         <>
             <div
-                className="flex flex-col md:flex-row items-start mx-auto my-5
-            border border-[#F5F5F7] shadow-2xl rounded-3xl bg-[#E6E6E610] w-full max-w-5xl p-4
-            hover:shadow-[0_0_20px_rgba(255,255,255,0.2)] transition-all duration-300 ease-in-out"
+                className="
+            flex flex-col md:flex-row items-start mx-auto my-5 bg-white/10 backdrop-blur-lg border
+            border-white/20 shadow-[0_8px_32px_0_rgba(31,38,135,0.37)]
+            rounded-3xl w-full max-w-5xl p-6 hover:shadow-[0_0_40px_rgba(255,255,255,0.4)]
+            hover:border-white/40
+            hover:-translate-y-1
+            transition-all duration-300 ease-in-out
+            "
             >
-                <div className="flex-shrink-0 w-64 h-64 overflow-hidden">
+                <div className="flex-shrink-0 w-64 h-64 overflow-hidden rounded-full">
                     <img src={logo} alt="Ceci n'est pas une image" className="w-full h-full object-contain" />
                 </div>
 
                 <div className="flex flex-col justify-between ml-2">
                     <div className="flex justify-between items-start my-0">
                         <h2 className="text-[35px] my-0 leading-none">{nom}</h2>
-                        <button className="hover:bg-[#e2910e] font-medium text-sm px-4 py-1 rounded-lg translate-y-1/8  ">More</button>
+
+                        <button
+                            className="hover:bg-[#58AEFF] font-medium text-sm px-4 py-1 rounded-lg translate-y-1/8
+                        border-blue-400 border-2"
+                        >
+                            More
+                        </button>
                     </div>
 
                     <p className="mt-2 text-sm leading-relaxed">{description}</p>
--- src/ui/components/clubs/clubslist.component.tsx
+++ src/ui/components/clubs/clubslist.component.tsx
@@ -2,8 +2,8 @@ import { FC, ReactNode } from "react";
 import ClubCard from "./clubscard.component";
 
 export interface Club {
-    nom        : string;
-    logo       : string;
+    nom: string;
+    logo: string;
     description: string;
 }
 
@@ -11,8 +11,8 @@ const CLUBS: Club[] = [
     // Replace this by the API call to gather all the clubs.
     {
         //
-        nom        : "CIA (Club d'Info et Algo)",
-        logo       : "/fillingPic.gif",
+        nom: "CIA (Club d'Info et Algo)",
+        logo: "/fillingPic.gif",
         description: `Vous aimez l'informatique et l'algorithmique et vous désirez aller plus loin que les notions abordées en cours dans ces domaines ? Alors le CIA (Club d'Informatique et d'Algorithmique) est l'endroit idéal pour vous.
 Le CIA propose des activités diverses et variées dans un cadre convivial pour ses membres, avec notamment des projets tels que la création d'une
 application d'emploi du temps pour Polytech, des défis de programmation comme des hackathons, ou encore la possibilité de participer à des concours internationaux tels que le SWERC, ou nationaux tels que Prologin.
@@ -20,8 +20,8 @@ application d'emploi du temps pour Polytech, des défis de programmation comme d
 Pour plus de détails, vous pouvez nous contacter via notre mail, sur le Discord du CIA, et nous retrouver tous les jeudis à partir de 14h en salle B007 du bâtiment 620 !`,
     },
     {
-        nom        : "CIA (Club d'Info et Algo)",
-        logo       : "/fillingPic.gif",
+        nom: "CIA (Club d'Info et Algo)",
+        logo: "/fillingPic.gif",
         description: `Vous aimez l'informatique et l'algorithmique et vous désirez aller plus loin que les notions abordées en cours dans ces domaines ? Alors le CIA (Club d'Informatique et d'Algorithmique) est l'endroit idéal pour vous.
 Le CIA propose des activités diverses et variées dans un cadre convivial pour ses membres, avec notamment des projets tels que la création d'une
 application d'emploi du temps pour Polytech, des défis de programmation comme des hackathons, ou encore la possibilité de participer à des concours internationaux tels que le SWERC, ou nationaux tels que Prologin.
@@ -29,8 +29,8 @@ application d'emploi du temps pour Polytech, des défis de programmation comme d
 Pour plus de détails, vous pouvez nous contacter via notre mail, sur le Discord du CIA, et nous retrouver tous les jeudis à partir de 14h en salle B007 du bâtiment 620 !`,
     },
     {
-        nom        : "CIA (Club d'Info et Algo)",
-        logo       : "/fillingPic.gif",
+        nom: "CIA (Club d'Info et Algo)",
+        logo: "/fillingPic.gif",
         description: `Vous aimez l'informatique et l'algorithmique et vous désirez aller plus loin que les notions abordées en cours dans ces domaines ? Alors le CIA (Club d'Informatique et d'Algorithmique) est l'endroit idéal pour vous.
 Le CIA propose des activités diverses et variées dans un cadre convivial pour ses membres, avec notamment des projets tels que la création d'une
 application d'emploi du temps pour Polytech, des défis de programmation comme des hackathons, ou encore la possibilité de participer à des concours internationaux tels que le SWERC, ou nationaux tels que Prologin.
