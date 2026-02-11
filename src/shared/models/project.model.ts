533e7b5be161753f4b4de83a53ed087c9ae6baa1
VoLtRL <raphael.lecaille@gmail.com>
2026-02-11 23:00:26 +0100

    feat(club-17): added club api call, club/project/event interface, modified club page

--- src/shared/models/project.model.ts
+++ src/shared/models/project.model.ts
@@ -1,26 +1,20 @@
 import { unknownToDate, unknownToNumber, unknownToString } from "../utils/common/convert.util";
 import { createConverter, createMapper } from "../utils/common/mapper.util";
 
-export interface Event {
+export interface Project {
     id            : number;
     club_id       : number;
     title         : string;
     date          : Date;
-    place         : string;
     description   : string;
-    capacity      : number;
-    others        : string;
     image_location: string;
 }
 
-export const mapEvent = createMapper<Event>({
+export const mapProject = createMapper<Project>({
     id            : createConverter(unknownToNumber, -1),
     club_id       : createConverter(unknownToNumber, -1), // eslint-disable-line @typescript-eslint/naming-convention
     title         : createConverter(unknownToString, ""),
     date          : createConverter(unknownToDate, new Date()),
-    place         : createConverter(unknownToString, ""),
     description   : createConverter(unknownToString, ""),
-    capacity      : createConverter(unknownToNumber, -1),
-    others        : createConverter(unknownToString, ""),
     image_location: createConverter(unknownToString, ""), // eslint-disable-line @typescript-eslint/naming-convention
 });
--- src/ui/components/clubs/clubslist.component.tsx
+++ src/ui/components/clubs/clubslist.component.tsx
@@ -1,45 +1,22 @@
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useEffect, useState } from "react";
 import ClubCard from "./clubscard.component";
-
-export interface Club {
-    nom: string;
-    logo: string;
-    description: string;
-}
-
-const CLUBS: Club[] = [
-    // Replace this by the API call to gather all the clubs.
-    {
-        //
-        nom: "CIA (Club d'Info et Algo)",
-        logo: "/fillingPic.gif",
-        description: `Vous aimez l'informatique et l'algorithmique et vous désirez aller plus loin que les notions abordées en cours dans ces domaines ? Alors le CIA (Club d'Informatique et d'Algorithmique) est l'endroit idéal pour vous.
-Le CIA propose des activités diverses et variées dans un cadre convivial pour ses membres, avec notamment des projets tels que la création d'une
-application d'emploi du temps pour Polytech, des défis de programmation comme des hackathons, ou encore la possibilité de participer à des concours internationaux tels que le SWERC, ou nationaux tels que Prologin.
-
-Pour plus de détails, vous pouvez nous contacter via notre mail, sur le Discord du CIA, et nous retrouver tous les jeudis à partir de 14h en salle B007 du bâtiment 620 !`,
-    },
-    {
-        nom: "CIA (Club d'Info et Algo)",
-        logo: "/fillingPic.gif",
-        description: `Vous aimez l'informatique et l'algorithmique et vous désirez aller plus loin que les notions abordées en cours dans ces domaines ? Alors le CIA (Club d'Informatique et d'Algorithmique) est l'endroit idéal pour vous.
-Le CIA propose des activités diverses et variées dans un cadre convivial pour ses membres, avec notamment des projets tels que la création d'une
-application d'emploi du temps pour Polytech, des défis de programmation comme des hackathons, ou encore la possibilité de participer à des concours internationaux tels que le SWERC, ou nationaux tels que Prologin.
-
-Pour plus de détails, vous pouvez nous contacter via notre mail, sur le Discord du CIA, et nous retrouver tous les jeudis à partir de 14h en salle B007 du bâtiment 620 !`,
-    },
-    {
-        nom: "CIA (Club d'Info et Algo)",
-        logo: "/fillingPic.gif",
-        description: `Vous aimez l'informatique et l'algorithmique et vous désirez aller plus loin que les notions abordées en cours dans ces domaines ? Alors le CIA (Club d'Informatique et d'Algorithmique) est l'endroit idéal pour vous.
-Le CIA propose des activités diverses et variées dans un cadre convivial pour ses membres, avec notamment des projets tels que la création d'une
-application d'emploi du temps pour Polytech, des défis de programmation comme des hackathons, ou encore la possibilité de participer à des concours internationaux tels que le SWERC, ou nationaux tels que Prologin.
-
-Pour plus de détails, vous pouvez nous contacter via notre mail, sur le Discord du CIA, et nous retrouver tous les jeudis à partir de 14h en salle B007 du bâtiment 620 !`,
-    },
-];
+import { getAllClubs } from "@/api/club.api";
+import { Club } from "@/shared/models/club.model";
 
 const ClubsListComp: FC = (): ReactNode => {
+    const [clubs, setClubs] = useState<Club[]>([]);
+
+    useEffect(() => {
+        getAllClubs()
+            .then((clubs) => {
+                setClubs(clubs);
+                console.log(clubs);
+            })
+            .catch((error: unknown) => {
+                console.error(error);
+            });
+    }, []);
+
     return (
         <>
             <div className="w-4/5 m-[2%]">
@@ -48,8 +25,8 @@ const ClubsListComp: FC = (): ReactNode => {
                 </div>
 
                 <div className="mt-[3%]">
-                    {CLUBS.map((club) => {
-                        return <ClubCard key={club.nom} {...club} />;
+                    {clubs.map((club) => {
+                        return <ClubCard club={club} />;
                     })}
                 </div>
             </div>
