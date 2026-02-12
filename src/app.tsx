ab18beb6f62d6a121cd49f52dd6f1dd602544923
VoLtRL <raphael.lecaille@gmail.com>
2026-02-12 17:52:24 +0100

    feat(club-18): update DetailedClubPage import path and clean up code

--- src/app.tsx
+++ src/app.tsx
@@ -11,8 +11,7 @@ const HomePage         = lazy(() => { return import("@/ui/pages/home/home.page")
 const NotFoundPage     = lazy(() => { return import("@/ui/pages/not_found.page"); });
 const ClubPage         = lazy(() => { return import("@/ui/pages/clubs/clubs.page"); });
 const MembersPage  = lazy(() => { return import("@/ui/pages/members/members.page"); });
-const DetailedClubPage = lazy(() => { return import("@/ui/pages/club/detailedclub.page"); });
-
+const DetailedClubPage = lazy(() => { return import("@/ui/pages/clubs/{name}/detailedclub.page"); });
 
 const App: FC = (): ReactNode => {
     useEffect(() => {
