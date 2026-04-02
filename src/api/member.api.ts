9ecace941d03e94b26d7e307e2ee9ac848e1792d
GabrielRadiguet <g.radiguet@proton.me>
2026-04-02 18:04:34 +0200

    fix(dev-34): fix some variable name errors

--- src/api/member.api.ts
+++ src/api/member.api.ts
@@ -2,19 +2,19 @@ import { Member, mapMember } from "@/shared/models/member.model";
 
 const MEMBERS: Member[] = [
     {
-        user_id    : 1,
-        club_id    : 1,
-        fonction_id: 1,
+        user_id: 1,
+        club_id: 1,
+        job_id : 1,
     },
     {
-        user_id    : 2,
-        club_id    : 2,
-        fonction_id: 2,
+        user_id: 2,
+        club_id: 2,
+        job_id : 2,
     },
     {
-        user_id    : 3,
-        club_id    : 3,
-        fonction_id: 3,
+        user_id: 3,
+        club_id: 3,
+        job_id : 3,
     },
 ];
 
